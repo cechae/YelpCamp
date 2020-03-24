56980ae19db387611c462ba1ef8842e4f1c25d00
spicysos <cchae216@gmail.com>
2020-03-23 22:05:12 -0400

    styling

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -15,23 +15,29 @@ router.get('/', function (req, res) {
 });
 
 // NEW route - Show the form
-router.get('/new', function (req, res) {
+router.get('/new', isLoggedIn, function (req, res) {
 	res.render('campgrounds/new');
 });
 
 // CREATE route
-router.post('/', function (req, res) {
+router.post('/', isLoggedIn, function (req, res) {
 	// get data from the form
 	let n = req.body.name;
 	let img = req.body.image;
 	let des = req.body.description;
-	// redirect back to campgrounds page
-	let obj = { name: n, image: img, description: des };
+	let author = {
+		id: req.user._id,
+		username: req.user.username
+	}
+	
+	let newCampground = { name: n, image: img, description: des, author:author };
+	
 	//create a new campground and save it to the mongodb
-	Campground.create(obj, function (error, newlyCreated) {
+	Campground.create(newCampground, function (error, newlyCreated) {
 		if (error) {
 			console.log(error);
 		} else {
+			console.log(newlyCreated)
 			res.redirect('/campgrounds');
 		}
 	});
@@ -46,5 +52,14 @@ router.get('/:id', function (req, res) {
 		}
 	});
 });
+
+
+// Middleware
+function isLoggedIn(req,res,next){
+	if (req.isAuthenticated()){
+		return next();
+	}
+	res.redirect("/login");
+}
 
 module.exports = router;
